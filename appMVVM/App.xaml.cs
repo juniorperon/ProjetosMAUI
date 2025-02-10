352712ab19319b22c0866ab67e79eac567aef07e
juniorperon <junior.peron@hotmail.com>
2025-02-09 23:23:07 -0300

    mvvm project

---
+++ appMVVM/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using appMVVM.Views;
+
+namespace appMVVM
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new StartPage();
+        }
+    }
+}
---
+++ appMVVM/Views/StartPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace appMVVM.Views;
+
+public partial class StartPage : ContentPage
+{
+	public StartPage()
+	{
+		InitializeComponent();
+	}
+}
